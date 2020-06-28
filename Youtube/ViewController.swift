94706f63f2d9079835a2ca024dda2520da4f70fa
lualexis-cl <59345460+lualexis-cl@users.noreply.github.com>
2020-06-28 16:11:29 -0400

    Initial Commit

---
+++ Youtube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube
+//
+//  Created by Luis Arredondo Andrade on 2020-06-28.
+//  Copyright © 2020 Luis Arredondo Andrade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
